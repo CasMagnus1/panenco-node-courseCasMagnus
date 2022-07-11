d6c5343c7306a55c0be5e972b07ac4e7986bc59e
CasMagnus1 <cas.magnus@student.kuleuven.be>
2022-07-11 14:34:53 +0200

    ts omgezet

--- src/controllers/users/handlers/create.handler.ts
+++ src/controllers/users/handlers/create.handler.ts
@@ -1,6 +1,7 @@
+import { NextFunction, Request, Response } from "express";
 import { UserStore } from "./user.store";
 
-export const createUser = async (req, res, next) => {
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     if (req.body.name == null) {
         return next({"details":"no user name provided"});
     }
